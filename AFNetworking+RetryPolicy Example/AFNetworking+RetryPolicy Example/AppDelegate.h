0c1678d91eb1eec3a66ea12653fec7414327b69d
Jakub Truhlar <kubatruhlar@gmail.com>
2016-09-28 20:50:39 +0200

    Add example project and travis.yml

---
+++ AFNetworking+RetryPolicy Example/AFNetworking+RetryPolicy Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking+RetryPolicy Example
+//
+//  Created by Jakub Truhlar on 28.09.16.
+//  Copyright © 2016 Jakub Truhlář. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
